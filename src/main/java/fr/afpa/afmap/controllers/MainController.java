3f367ecf33ebb40fd3d3fefd1fa810220c2d30f8
Antonin <antonin.lop1@gmail.com>
2023-02-28 10:59:44 +0100

    Je pousse un truc qui marche plus mais le x1 est bien paramétrer

--- src/main/java/fr/afpa/afmap/controllers/MainController.java
+++ src/main/java/fr/afpa/afmap/controllers/MainController.java
@@ -7,6 +7,7 @@ import fr.afpa.afmap.model.Formation;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ComboBox;
@@ -15,6 +16,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -47,6 +50,11 @@ public class MainController {
     @FXML
     private Pane pane;
 
+    @FXML
+    private Canvas mainCanvas;
+
+    @FXML
+    private Group drawingGroup;
 
 
 //dfdbf
@@ -57,10 +65,8 @@ public class MainController {
     private ArrayList<Canvas> canvasArrayList = new ArrayList<>();
 
 
-    static GraphicsContext gc;
-
     //ajout batiments
-    BatimentFormation batFor1 = new BatimentFormation(64, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+    BatimentFormation batFor1 = new BatimentFormation(64, 22.21, 9.55, 27.41, 9.55, 22.21, 13.10, 27.41, 13.10);
     BatimentFormation batFor2 = new BatimentFormation(125, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
 
     //ajout formateurs
@@ -74,15 +80,42 @@ public class MainController {
     List<String> typeBatimentListe = new ArrayList<String>();
     List<Formation> formationList = new ArrayList<Formation>();
 
+    Double width;
+
 
     public void initialize() {
-        pane.widthProperty().addListener((obs, oldVal, newVal)->{
+
+        Rectangle shapeCDA = new Rectangle();
+
+
+//  Main Canvas Fill
+        GraphicsContext gc = mainCanvas.getGraphicsContext2D();
+
+
+//        LISTENER WIDTH
+        pane.widthProperty().addListener((obs, oldVal, newVal) -> {
             imageViewBat.fitWidthProperty().bind(pane.widthProperty());
+            System.out.println(pane.getHeight());
+            width = newVal.doubleValue();
+
+            Shape shape = changePlaceRectangle(newVal.doubleValue(), 22.21, 14.35 , Math.round(newVal.doubleValue() / 1.51), shapeCDA, 1536, 1014);
+            drawingGroup.getChildren().add(shape);
+
         });
+
+//        LISTENER HEIGHT
+        pane.heightProperty().addListener((obs, oldVal, newVal) -> {
+            mainCanvas.setWidth(width);
+            mainCanvas.setHeight(newVal.doubleValue());
+        });
+
+
         batFor1.addFormation(cda);
         batFor2.addFormation(electricien);
         batimentFormationArrayList.add(batFor1);
         batimentFormationArrayList.add(batFor2);
+
+
         //recuperer toutes les formations de tous les batiments_formation et les afficher dans la comboFormation
         for (BatimentFormation bat : batimentFormationArrayList) {
             List<Formation> formationsDuBatiment = bat.getListeFormations();
@@ -102,7 +135,8 @@ public class MainController {
         labelFormation.setVisible(false);
 //
 //
-//    Canvas canvaCDA = new Canvas(22.55);
+
+
 //        canvasArrayList.add(canvaCDA);
 //        canvasArrayList.add(canvaCDABis);
 
@@ -135,7 +169,14 @@ public class MainController {
 //                canvaCDABis.setOpacity(0);
 //            }
         });
-
+//
+//        pane.setOnMouseMoved(mouseEvent -> {
+//            System.out.println("---------------------");
+//            System.out.println("Mouse en X : " + mouseEvent.getX());
+//            System.out.println("Mouse en Y : " + mouseEvent.getY());
+//            System.out.println("Taille de l'image :" + imageViewBat.fitWidthProperty().get());
+//            System.out.println("Hauteur de l'image : " + pane.getHeight());
+//        });
 
 
 //        EVENT ON CDA BAT
@@ -165,8 +206,39 @@ public class MainController {
 //        });
 
 
-
     }
+
+
+    public static double getLayout(double x, double reso) {
+        return x * reso / 100;
+    }
+
+
+    public static Shape changePlaceRectangle(double newReso, double x, double y, double heigthReso, Rectangle rectangle, double oldReso, double oldResoHeigth) {
+
+//
+
+//        rectangle.
+        double reso = 0;
+        double resoHeigth = 0;
+        if (oldReso != newReso) {
+            reso = Math.floor(newReso * x / 100) - Math.floor(oldReso * x / 100);
+            resoHeigth = Math.floor(heigthReso * y / 100) - Math.floor(oldResoHeigth * y / 100);
+
+        }
+
+
+        rectangle.setLayoutY(resoHeigth);
+        rectangle.setLayoutX(reso);
+
+        rectangle.setFill(Color.RED);
+
+
+        return rectangle;
+
+
+
+}
 
 
 }
