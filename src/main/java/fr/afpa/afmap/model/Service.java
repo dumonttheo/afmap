45c336865aec8ed54ea0e4240ba7f87045af09cb
Antonin <antonin.lop1@gmail.com>
2023-03-01 16:43:25 +0100

    Ajout de la JavaDoc dans la classe Service et Personnel

--- src/main/java/fr/afpa/afmap/model/Service.java
+++ src/main/java/fr/afpa/afmap/model/Service.java
@@ -2,26 +2,44 @@ package fr.afpa.afmap.model;
 
 import java.util.ArrayList;
 
+/**
+ *  Service class to go whit Administrativ Building
+ */
 public class Service {
     private String nom;
     private ArrayList<Personnel> listePersonnel = new ArrayList<>();
 
+    /**
+     * @param nom Name of service
+     */
     public Service(String nom){
         this.nom = nom;
     }
 
+    /**
+     * @return Get name of this service
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * @param nom set name of this service
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * @param personnel Add personnel to ArrayList of Personnal
+     */
     public void addPersonnel(Personnel personnel){
         listePersonnel.add(personnel);
     }
 
+    /**
+     * @return ArrayList of Personnel
+     */
     public ArrayList<Personnel> getListePersonnel() {
         return listePersonnel;
     }
